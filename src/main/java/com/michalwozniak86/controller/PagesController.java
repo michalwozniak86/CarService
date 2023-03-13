6d23064541cf4736525193ca92282981bb16facd
Michał Woźniak <michal.wozniak86@int.pl>
2023-03-13 19:12:04 +0100

    add new controllers add thymeleaf

--- src/main/java/com/michalwozniak86/controller/PagesController.java
+++ src/main/java/com/michalwozniak86/controller/PagesController.java
@@ -10,4 +10,29 @@ public class PagesController {
     public String index() {
         return "index";
     }
+
+    @GetMapping("/index")
+    public String indexPage() {
+        return "index";
+    }
+
+    @GetMapping("/add-car")
+    public String addCar() {
+        return "add-car";
+    }
+
+    @GetMapping("/cars-to-repair")
+    public String carsToRepair() {
+        return "cars-to-repair";
+    }
+
+    @GetMapping("/repair-car")
+    public String repairCar() {
+        return "repair-car";
+    }
+
+    @GetMapping("/repaired-cars")
+    public String repairedCars() {
+        return "repaired-cars";
+    }
 }
