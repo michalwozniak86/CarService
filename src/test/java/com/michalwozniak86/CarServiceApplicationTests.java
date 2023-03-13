42beb2e7927e62b16624e64de8b91f356e832cb9
Michał Woźniak <michal.wozniak86@int.pl>
2023-03-13 15:17:07 +0100

    Initialization commit

---
+++ src/test/java/com/michalwozniak86/CarServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michalwozniak86;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
